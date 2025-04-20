5f97fd755a961277a6a4c8a43b9be78cc2a0650b
AlliedWebapp <alliedcunsultant@gmail.com>
2025-04-21 00:59:50 +0530

    api

--- routes/ReportRoutes.js
+++ routes/ReportRoutes.js
@@ -36,7 +36,7 @@ router.post(
 );
 
 // ✅ New GET route for improvement reports
-router.get("s/improvementreports", getAllImprovementReports);
+router.get("/api/reports/view-improvement-reports", getAllImprovementReports);
 
 // 🚧 Maintenance Report Route
 router.post(
